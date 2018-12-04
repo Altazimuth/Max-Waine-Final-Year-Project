4e0aaa657b9f117a1911ac132bbcab7bedfff968
Max Waine <bobishmax@hotmail.com>
2018-12-04 10:49:50 +0000

    Save me

--- source/main.cpp
+++ source/main.cpp
@@ -8,7 +8,9 @@
 #include "SDL.h"
 #include "SDL_opengl.h"
 
-#include "IL/il.h"
+#include <IL/il.h>
+#include <IL/ilu.h>
+#include <IL/ilut.h>
 
 #include "shader_fragment.h"
 #include "shader_vertex.h"
@@ -35,6 +37,77 @@ static GLuint gIBO = 0;
 static ILint    linearheight;
 static ILint    linearwidth;
 static uint8_t *linearimage;
+
+static GLuint IL_loadImage(const char* theFileName)
+{
+   ILuint imageID;    // Create an image ID as a ULuint
+   GLuint textureID;  // Create a texture ID as a GLuint
+   ILboolean success; // Create a flag to keep track of success/failure
+   ILenum error;      // Create a flag to keep track of the IL error state
+
+   ilGenImages(1, &imageID);           // Generate the image ID
+   ilBindImage(imageID);               // Bind the image
+   success = ilLoadImage(theFileName); // Load the image file
+
+   // If we managed to load the image, then we can start to do things with it...
+   if(success)
+   {
+      // If the image is flipped (i.e. upside-down and mirrored, flip it the right way up!)
+      ILinfo ImageInfo;
+      iluGetImageInfo(&ImageInfo);
+      if(ImageInfo.Origin == IL_ORIGIN_UPPER_LEFT)
+         iluFlipImage();
+
+      // Convert the image into a suitable format to work with
+      // NOTE: If your image contains alpha channel you can replace IL_RGB with IL_RGBA
+      success = ilConvertImage(IL_RGB, IL_UNSIGNED_BYTE);
+
+      // Quit out if we failed the conversion
+      if(!success)
+      {
+         error = ilGetError();
+         printf("Image conversion failed - IL reports error: %d - %s\n", error, iluErrorString(error));
+         exit(-1);
+      }
+
+      // Generate a new texture
+      glGenTextures(1, &textureID);
+
+      // Bind the texture to a name
+      glBindTexture(GL_TEXTURE_2D, textureID);
+
+      // Set texture clamping method
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+      // Set texture interpolation method to use linear interpolation (no MIPMAPS)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+      // Specify the texture specification
+      glTexImage2D(GL_TEXTURE_2D,                  // Type of texture
+                   0,                              // Pyramid level (for mip-mapping) - 0 is the top level
+                   ilGetInteger(IL_IMAGE_FORMAT),  // Internal pixel format to use. Can be a generic type like GL_RGB or GL_RGBA, or a sized type
+                   ilGetInteger(IL_IMAGE_WIDTH),   // Image width
+                   ilGetInteger(IL_IMAGE_HEIGHT),  // Image height
+                   0,                              // Border width in pixels (can either be 1 or 0)
+                   ilGetInteger(IL_IMAGE_FORMAT),  // Format of image pixel data
+                   GL_UNSIGNED_BYTE,               // Image data type
+                   ilGetData());                   // The actual image data itself
+   }
+   else // If we failed to open the image file in the first place...
+   {
+      error = ilGetError();
+      printf("Image load failed - IL reports error: %d - %s\n", error, iluErrorString(error));
+      exit(-1);
+   }
+
+   ilDeleteImages(1, &imageID); // Because we have already copied image data into texture data we can release memory used by image.
+
+   puts("Texture creation successful.");
+
+   return textureID; // Return the GLuint to the texture so you can use it!
+}
 
 // Shader loading utility programs
 
@@ -211,21 +284,13 @@ static bool initGL()
    return success;
 }
 
-static void IL_init(const char *filename)
+static void IL_init()
 {
-   ILuint ImgId = 0;
-   ilGenImages(1, &ImgId);
-   ilBindImage(ImgId);
-
-   ilLoadImage(filename);
-
-   linearwidth    = ilGetInteger(IL_IMAGE_WIDTH);
-   linearheight   = ilGetInteger(IL_IMAGE_HEIGHT);
-   uint8_t *image = new uint8_t[linearwidth * linearheight * 3];
-   ilCopyPixels(0, 0, 0, linearwidth, linearheight, 1, IL_RGB, IL_UNSIGNED_BYTE, linearimage);
-
-   ilBindImage(0);
-   ilDeleteImage(ImgId);
+   ilutRenderer(ILUT_OPENGL);
+   ilInit();
+   iluInit();
+   ilutInit();
+   ilutRenderer(ILUT_OPENGL);
 }
 
 //
@@ -365,7 +430,7 @@ static void close()
 #undef main
 #endif
 
-int main(int argc, char *args[])
+int main(int argc, char *argv[])
 {
    // Load image into linear
    if(argc < 0)
@@ -374,7 +439,9 @@ int main(int argc, char *args[])
       return -1;
    }
 
-   IL_init(args[1]);
+   IL_init();
+
+   IL_loadImage(argv[1]);
 
    // Start up SDL and create window
    if(!init())
